012df1dbb86d094a5b3d22fe356ca696480cd788
Kerwin <109135319+KerwinFuyihang@users.noreply.github.com>
2024-07-09 23:07:18 +0800

    Update data_out.py

--- Data/Algorand_Data_Inputs/data_out.py
+++ Data/Algorand_Data_Inputs/data_out.py
@@ -1,17 +1,17 @@
 import csv
 
-# 读取CSV文件
+# Read cvs file
 with open('al_block_data_proposercount_reward.csv', 'r') as file:
     csv_reader = csv.reader(file)
-    header = next(csv_reader)  # 跳过标题行
-    column_index = 1  # 假设要导出第三列数据，索引从0开始
+    header = next(csv_reader)  
+    column_index = 1  
 
-    # 提取指定列数据
+    # read column data
     column_data = [row[column_index] for row in csv_reader]
 
-    # 转换为逗号分隔的字符串
+    # comma seperated
     csv_string = ','.join(column_data)
 
-    # 导出数据
+    # output
     with open('output_proposer.txt', 'w') as output_file:
         output_file.write(csv_string)
